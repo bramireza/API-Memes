95dbfe84eab8495ec1b9ce5ddb451d4d3588164a
Brayian <bramirezag@gmail.com>
2023-04-30 23:35:29 -0500

    [feat] add structure for API

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+app.use(cors());
+
+module.exports = app;
